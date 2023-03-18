bbaa04bba17929c0c5dcbce8e0356e21a21e6413
rohin <rs.rohinsood@gmail.com>
2023-03-18 00:32:53 -0700

    profile init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot class="font-poppins"/>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.slate.50);
+    overflow: hidden;
+  }
+</style>
